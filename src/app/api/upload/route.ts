6ac18e29e24a1ce20c9ace6b319303def8429559
fuerte <leandrofuerte75@gmail.com>
2025-04-01 18:33:05 +0100

    putting hover in button

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -1,12 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
-  const body = await request.json();
-
-  const { file } = body;
-
-  console.log(body);
-  if (!file) {
+  const searchParams = new URL(request.url).searchParams;
+  const filename = searchParams.get("file");
+  console.log(filename);
+  if (!filename) {
     return NextResponse.json({ message: "No file uploaded" }, { status: 400 });
   }
   return NextResponse.json({ message: "File uploaded successfully" });
