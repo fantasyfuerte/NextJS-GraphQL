ffd6832f72c83cc9c8b967823f52b77135e7ec50
fuerte <leandrofuerte75@gmail.com>
2025-04-01 16:45:19 +0100

    Removing all styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,37 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify- gap-5">
-      <h1 className="text-3xl font-bold underline">NextJS GraphQL</h1>
-      <p className="text-xl">
-        NextJS & GraphQL is a great combination for building modern web
-        applications.
-      </p>
-      <img
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSg1rLbeOW7TGvpK6jxXzyfTJiT7nN3oKa1w&s"
-        alt="NextJS GraphQL"
-        width={500}
-        height={300}
-      />
-      <p className="text-xl">
-        This project is a simple example of how to use GraphQL with NextJS.
-      </p>
-      <p className="text-xl">
-        <a
-          className="text-blue-500 underline"
-          href="https://nextjs.org/docs/basic-features/data-fetching"
-        >
-          NextJS Data Fetching
-        </a>
-      </p>
-      <p className="text-xl">
-        <a
-          className="text-blue-500 underline"
-          href="https://graphql.org/learn/"
-        >
-          GraphQL
-        </a>
-      </p>
+    <main>
+      <section>
+        <h1 className="text-xl p-4 text-emerald-400 font-bold">NextJS + GraphQL</h1>
+      </section>
     </main>
   );
 }
