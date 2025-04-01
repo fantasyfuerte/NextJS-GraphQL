9c4c9fddcab2f649237300fc0f2d49a222d427e0
fuerte <leandrofuerte75@gmail.com>
2025-04-01 18:14:29 +0100

    styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,11 @@ import FileUploader from "./components/file-uploader";
 export default function Home() {
   return (
     <main>
-      <section>
+      <section className="mt-16">
         <h1 className="text-xl text-center p-4 text-emerald-400 font-bold">
           NextJS + GraphQL
         </h1>
-        <article className="text-center p-10 mx-40 lg:mx-64 my-5 border-emerald-400 border-b">
+        <article className="text-center p-10 my-5 border-emerald-400 border-b w-1/2 mx-auto">
           <FileUploader />
         </article>
       </section>
