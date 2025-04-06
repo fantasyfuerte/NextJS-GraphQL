55ab6a67503091895222ed3b97f209023a968126
fuerte <leandrofuerte75@gmail.com>
2025-04-06 13:02:11 +0100

    Creating server to recieve data

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -1,12 +1,11 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
-  const files = (await request.formData()).getAll("file");
+  
+  const formData = await request.formData()
+  console.log(formData.getAll("file"))
 
-  console.log(files);
+  
 
-  if (!files) {
-    return NextResponse.json({ message: "No file uploaded" }, { status: 400 });
-  }
   return NextResponse.json({ message: "File uploaded successfully" });
 }
